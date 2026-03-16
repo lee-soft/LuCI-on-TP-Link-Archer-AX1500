d3ff313c06d43212c7a66b2a9b94d708c42079fd
Lee Chantrey <lchantrey@gmail.com>
2026-03-16 07:59:28 +0000

    use repo zip instead of release

--- setup.sh
+++ setup.sh
@@ -4,15 +4,18 @@ kill -9 $(pgrep uhttpd_vanilla) 2>/dev/null
 kill -9 $(pgrep dropbear_vanilla) 2>/dev/null
 sleep 1
 
-echo "Downloading luci-deploy.zip..."
-curl -4 -L -k -o /tmp/luci-deploy.zip https://github.com/lee-soft/LuCI-on-TP-Link-Archer-AX1500/releases/download/v1.0/luci-deploy.zip
+echo "Downloading repo zip..."
+curl -4 -L -k -o /tmp/luci-deploy.zip https://github.com/lee-soft/LuCI-on-TP-Link-Archer-AX1500/archive/refs/heads/main.zip
 
 echo "Extracting..."
 cd /tmp
 unzip -o luci-deploy.zip
 
-echo "Deploying lua files..."
-cp -r /tmp/luci-lua/* /usr/lib/lua/luci/
+echo "Moving files from container folder..."
+cp -r /tmp/LuCI-on-TP-Link-Archer-AX1500-main/luci-lua/* /usr/lib/lua/luci/
+cp -r /tmp/LuCI-on-TP-Link-Archer-AX1500-main/www-vanilla /tmp/www-vanilla
+cp /tmp/LuCI-on-TP-Link-Archer-AX1500-main/uhttpd_vanilla /tmp/uhttpd_vanilla
+cp /tmp/LuCI-on-TP-Link-Archer-AX1500-main/dropbear_vanilla /tmp/dropbear_vanilla
 
 echo "Fixing permissions..."
 chmod +x /tmp/www-vanilla/cgi-bin/luci
@@ -31,6 +34,10 @@ uci set luci.themes=internal
 uci set luci.themes.OpenWrt=/luci-static/openwrt.org
 uci commit luci
 sed -i '/killersteel/d' /etc/config/luci
+
+echo "Clearing LuCI cache..."
+rm -f /tmp/luci-indexcache
+rm -f /tmp/luci-modulecache/*
 
 echo "Starting dropbear SSH on port 2222..."
 (/tmp/dropbear_vanilla -p 2222 -R -E </dev/null >/dev/null 2>&1) &
